70469f02650e592a795c9728352f6e44ce386cd5
Rashed Talukder <>
2020-12-28 07:25:16 -0800

    Removed crypto auth registration utilities from Smart Thermostat

--- Smart-Thermostat/utilities/cryptoauth_trustplatform_designsuite/assets/winc_firmware_upgrade/winc_firmware_update.bat
+++
@@ -1,6 +0,0 @@
-@ECHO Off
-cd firmware
-for /f "usebackq" %%B in (`wmic path Win32_SerialPort Where "Caption LIKE '%%Curiosity%%'" Get DeviceID ^| FIND "COM"`) do set comport=%%B
-
-download_all_sb.bat nedbg ATSAMD21E18A Tools\serial_bridge\winc_serial_bridge.elf  3A0 0 0 %comport:~3%
-pause
