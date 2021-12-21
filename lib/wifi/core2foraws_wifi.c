93cc7b5239258dd2e2e55b1db229978af7a27ff2
Rashed Talukder <9218468+rashedtalukder@users.noreply.github.com>
2021-12-20 16:22:48 -0800

    Add LED pause macro for updated LED strip driver

--- lib/wifi/core2foraws_wifi.c
+++ lib/wifi/core2foraws_wifi.c
@@ -1,21 +1,31 @@
 /*
- * AWS IoT EduKit - Core2 for AWS IoT EduKit
- * Cloud Connected Blinky v1.4.0
- * wifi.c
- * 
- * Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * Additions Copyright 2016 Espressif Systems (Shanghai) PTE LTD
+ * Core2 for AWS IoT EduKit BSP v2.0.0
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
  *
- *  http://aws.amazon.com/apache2.0
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://aws.amazon.com/iot/edukit
+ *
+ */
+
+/**
+ * @file core2foraws_wifi.c
+ * @brief Core2 for AWS IoT EduKit Wi-Fi helper APIs
  */
 
 #include <string.h>
